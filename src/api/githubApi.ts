52df183376b0d815c390e8c21095836806e56524
JL-OLEMAR <joseluis19963@gmail.com>
2023-06-09 19:34:49 -0500

    chore: Add headers at API

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,6 +1,10 @@
 import axios from 'axios'
 
+const GithubToken: string = import.meta.env.VITE_GITHUB_TOKEN ?? ''
+
 export const githubApi = axios.create({
   baseURL: 'https://api.github.com/repos/facebook/react',
-  headers: {}
+  headers: {
+    Authorization: `Bearer ${GithubToken}`
+  }
 })
