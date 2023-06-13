c2272c554aa56f668cdcb29e2fae7389ec6f378b
JL-OLEMAR <joseluis19963@gmail.com>
2023-06-13 00:46:48 -0500

    feat: Display Issue and comments by issueNumber

--- src/issues/hooks/useIssue.ts
+++ src/issues/hooks/useIssue.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { useQuery } from '@tanstack/react-query'
 import { githubApi } from '../../api/githubApi'
 import { sleep } from '../../helpers/sleep'
@@ -9,11 +10,28 @@ const getIssueInfo = async (issueNumber: number): Promise<Issue> => {
   return data
 }
 
+const getIssueComments = async (issueNumber: number): Promise<Issue[]> => {
+  await sleep(2)
+  const { data } = await githubApi.get<Issue[]>(`/issues/${issueNumber}/comments`)
+  return data
+}
+
 export function useIssue(issueNumber: number) {
   const issueQuery = useQuery(
-    ['issue', issueNumber],
+    ['issue_by', issueNumber], // Name at devtools
     () => getIssueInfo(issueNumber)
   )
 
-  return { issueQuery }
+  const commentsQuery = useQuery(
+    ['issue_by', issueNumber, 'comments'], // Name at devtools
+    () => getIssueComments(issueQuery.data!.number),
+    {
+      enabled: Boolean(issueQuery.data)
+    }
+  )
+
+  return {
+    commentsQuery,
+    issueQuery
+  }
 }
