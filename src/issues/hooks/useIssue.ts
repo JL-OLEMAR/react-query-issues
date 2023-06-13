b2d70e2d25dff29911eaaff76978795f5ce84e21
JL-OLEMAR <joseluis19963@gmail.com>
2023-06-12 20:07:48 -0500

    feat: Add useIssue hook

---
+++ src/issues/hooks/useIssue.ts
@@ -0,0 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
+import { githubApi } from '../../api/githubApi'
+import { sleep } from '../../helpers/sleep'
+import { Issue } from '../interfaces'
+
+const getIssueInfo = async (issueNumber: number): Promise<Issue> => {
+  await sleep(2)
+  const { data } = await githubApi.get<Issue>(`/issues/${issueNumber}`)
+  return data
+}
+
+export function useIssue(issueNumber: number) {
+  const issueQuery = useQuery(
+    ['issue', issueNumber],
+    () => getIssueInfo(issueNumber)
+  )
+
+  return { issueQuery }
+}
